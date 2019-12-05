873b2a87bd72beed4ad1e52260c691c900d2c729
ivan <ivanwolyniec@gmail.com>
2019-12-05 16:30:27 -0500

    added search by name API functionality to cocktail-api.service

--- src/app/services/cocktail-api.service.ts
+++ src/app/services/cocktail-api.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
-const API_KEY;
+const API_KEY = 9973533;
 
 @Injectable({
   providedIn: "root"
 })
 export class CocktailAPIService {
   constructor(private http: HttpClient) {}
+
+  getCocktailList(searchTerm: string): Observable<any> {
+    return this.http.get(
+      `https://www.thecocktaildb.com/api/json/v2/${API_KEY}/search.php?`,
+      {
+        params: {
+          s: searchTerm
+        }
+      }
+    );
+  }
 }
