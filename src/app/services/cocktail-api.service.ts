1c542fa8e21514d3883c0234d3fb60dc8c3fb880
ivan <ivanwolyniec@gmail.com>
2019-12-05 16:33:27 -0500

    added search by category and ingredient methods

--- src/app/services/cocktail-api.service.ts
+++ src/app/services/cocktail-api.service.ts
@@ -10,12 +10,32 @@ const API_KEY = 9973533;
 export class CocktailAPIService {
   constructor(private http: HttpClient) {}
 
-  getCocktailList(searchTerm: string): Observable<any> {
+  searchByName(searchTerm: string): Observable<any> {
     return this.http.get(
       `https://www.thecocktaildb.com/api/json/v2/${API_KEY}/search.php?`,
       {
         params: {
           s: searchTerm
+        }
+      }
+    );
+  }
+  searchByCategory(searchTerm: string): Observable<any> {
+    return this.http.get(
+      `https://www.thecocktaildb.com/api/json/v2/${API_KEY}/search.php?`,
+      {
+        params: {
+          c: searchTerm
+        }
+      }
+    );
+  }
+  searchByIngredient(searchTerm: string): Observable<any> {
+    return this.http.get(
+      `https://www.thecocktaildb.com/api/json/v2/${API_KEY}/search.php?`,
+      {
+        params: {
+          i: searchTerm
         }
       }
     );
