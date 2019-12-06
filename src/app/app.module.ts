2bee27a3f1f10f49095cbae16046befe513731c1
ivan <ivanwolyniec@gmail.com>
2019-12-06 10:50:19 -0500

    search by name functionality currently working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SearchComponent } from './search/search.component';
-import { NotesComponent } from './notes/notes.component';
-import { SavedComponent } from './saved/saved.component';
-import { SignUpInComponent } from './sign-up-in/sign-up-in.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { SearchComponent } from "./search/search.component";
+import { NotesComponent } from "./notes/notes.component";
+import { SavedComponent } from "./saved/saved.component";
+import { SignUpInComponent } from "./sign-up-in/sign-up-in.component";
 
 @NgModule({
   declarations: [
@@ -18,11 +19,8 @@ import { SignUpInComponent } from './sign-up-in/sign-up-in.component';
     SavedComponent,
     SignUpInComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CocktailAPIService } from "../services/cocktail-api.service";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"]
 })
 export class SearchComponent implements OnInit {
+  constructor(private cocktailAPIService: CocktailAPIService) {}
+  searchType: string = "Name";
+  searchTerm: string = "";
+  resultsList: any[];
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  cocktailSearch() {
+    if (this.searchType === "Name") {
+      // search by name
+      this.resultsList = [];
+      this.cocktailAPIService.searchByName(this.searchTerm).subscribe(data => {
+        if (data.drinks !== null) {
+          for (let cocktail of data.drinks) {
+            this.resultsList.push({
+              name: cocktail.strDrink,
+              img: cocktail.strDrinkThumb,
+              id: cocktail.idDrink
+            });
+          }
+        }
+      });
+    } else if (this.searchType === "Ingredient") {
+      let cleanSearch = this.cleanIngredientsList();
+      //search by ingredient using cleanSearch
+    } else if (this.searchType === "Category") {
+      //search by category
+    } else {
+      alert("you shouldn't have gotten here");
+    }
   }
 
+  placeHolder() {
+    // TO DO: Cycle through a couple placeholders (on init?)
+    return "negroni";
+  }
+
+  cleanIngredientsList() {
+    // TO DO: clean up user input to fit it to the API
+  }
 }
