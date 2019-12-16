433425bebd09bbaa8009e6184a8e010517d7e7da
reuelethan <reuelbeats@gmail.com>
2019-12-16 14:28:00 -0500

    ??

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -32,7 +32,7 @@ export class SearchComponent implements OnInit {
   noResults: boolean = false;
 
   cocktailSearch() {
-    debugger;
+
     if (this.searchType === "Name") {
       // search by name
       this.resultsList = [];
